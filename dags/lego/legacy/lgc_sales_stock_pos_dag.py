15905b18d85cff5a52e59905624a9600e9e11a85
cn8XieZh <xie.zhenmao@consultant.lego.com>
2021-03-02 12:22:37 +0800

    fix bug: input csv has header

--- dags/lego/legacy/lgc_sales_stock_pos_dag.py
+++ dags/lego/legacy/lgc_sales_stock_pos_dag.py
@@ -82,7 +82,7 @@ def load_stg2ods(**kwargs):
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
-    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db, has_head = has_head )
+    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db, has_head = True )
     stg2ods.start()
 
 
