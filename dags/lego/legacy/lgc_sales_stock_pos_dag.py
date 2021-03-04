1c65b80435dfc94dc9b72f5f0fd890443a68df69
cn8XieZh <xie.zhenmao@consultant.lego.com>
2021-03-04 19:21:43 +0800

    test zip, legacy - round 18

--- dags/lego/legacy/lgc_sales_stock_pos_dag.py
+++ dags/lego/legacy/lgc_sales_stock_pos_dag.py
@@ -70,7 +70,7 @@ def load_src2stg(**kwargs):
     #
     OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
     excel_fun_list = [myutil.filter_modified_product, myutil.rearrange_columns]
-    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, sheetname='POS', merge =False)
+    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, has_head=False, sheetname='POS', merge =False)
     src2stg.start(version='v2')
 
 def load_stg2ods(**kwargs):
