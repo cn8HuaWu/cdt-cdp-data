9651711df01fb472f03a21449285fe4dba855ea7
cn8XieZh <xie.zhenmao@consultant.lego.com>
2021-03-04 13:11:44 +0800

    Test ZIP file with mutilple template, round 1

--- dags/lego/legacy/lgc_sales_stock_pos_dag.py
+++ dags/lego/legacy/lgc_sales_stock_pos_dag.py
@@ -70,7 +70,7 @@ def load_src2stg(**kwargs):
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    excel_fun_list = [myutil.filter_modified_product]
+    excel_fun_list = [myutil.filter_modified_product, myutil.rearrange_columns]
     src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, sheetname='POS')
     src2stg.start(version='v2')
 
