47af86578461d0d267a88f0adbda806cd3fdbb62
cn8XieZh <76941129+cn8XieZh@users.noreply.github.com>
2021-03-08 16:13:32 +0800

    Add files via upload
    new vison

--- dags/lego/legacy/lgc_sales_stock_rrp_rmb_dag.py
+++ dags/lego/legacy/lgc_sales_stock_rrp_rmb_dag.py
@@ -22,22 +22,17 @@ STAGING = 'Staging'
 ODS = 'ODS'
 TEMP_FOLDER='Temp'
 
-myutil = Myutil(DAG_HOME)
-gp_host = myutil.get_conf( 'Greenplum', 'GP_HOST')
-gp_port = myutil.get_conf( 'Greenplum', 'GP_PORT')
-gp_db = myutil.get_conf( 'Greenplum', 'GP_DB')
-gp_usr = myutil.get_conf( 'Greenplum', 'GP_USER')
-gp_pw = myutil.get_conf( 'Greenplum', 'GP_PASSWORD')
-db = Mydb(gp_host, gp_port, gp_db, gp_usr, gp_pw)
-entity_conf = myutil.get_entity_config()
-email_to_list =  Variable.get('email_to_list').split(',')
 entity = 'sales_stock_rrp_rmb'
 src_entity = 'lgc_sales_stock_rrp_rmb'
 DAG_NAME = 'lgc_sales_stock_rrp_rmb_dag'
 
-sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+myutil = Myutil(dag_home=DAG_HOME, entity_name=src_entity)
+db = myutil.get_db()
+entity_conf = myutil.get_entity_config()
+email_to_list =  Variable.get('email_to_list').split(',')
 
 def process_fileload(is_encrypted = False, is_compressed = False, **kwargs):
+    logging.info("current path: " + os.getcwd())
     OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
     
     # remove the ok file and get the source file
@@ -74,7 +69,8 @@ def load_src2stg(**kwargs):
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, sheetname='Sheet1')
+    excel_fun_list = [myutil.filter_modified_product, myutil.rearrange_columns]
+    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, has_head=False, sheetname='sheet1', merge =False)
     src2stg.start(version='v2')
 
 def load_stg2ods(**kwargs):
@@ -105,21 +101,22 @@ dag = DAG(dag_id = DAG_NAME,
             schedule_interval = None)
 
 preprocess_sales_stock_rrp_rmb_task = PythonOperator(
-    task_id='preprocess_sales_stock_rrp_rmb_task',
-    provide_context=True,
-    python_callable=process_fileload,
-    op_kwargs={'is_encrypted': False},
-    on_failure_callback=dag_failure_handler,
-    dag=dag,
+    task_id = 'preprocess_sales_stock_rrp_rmb_task',
+    provide_context = True,
+    python_callable = process_fileload,
+    op_kwargs = {'is_encrypted': False},
+    on_failure_callback = dag_failure_handler,
+    dag = dag,
 )
 
 sales_stock_rrp_rmb_src2stg_task = PythonOperator(
     task_id='sales_stock_rrp_rmb_src2stg_task',
-    provide_context=True,
-    python_callable=load_src2stg,
-    on_failure_callback=dag_failure_handler,
+    provide_context = True,
+    python_callable = load_src2stg,
+    on_failure_callback = dag_failure_handler,
     dag=dag,
 )
+
 
 sales_stock_rrp_rmb_stg2ods_task = PythonOperator(
     task_id='sales_stock_rrp_rmb_stg2ods_task',
@@ -129,6 +126,7 @@ sales_stock_rrp_rmb_stg2ods_task = PythonOperator(
     dag=dag,
 )
 
+
 postprocess_sales_stock_rrp_rmb_task = PythonOperator(
     task_id = 'postprocess_sales_stock_rrp_rmb_task',
     provide_context = True,
@@ -137,7 +135,5 @@ postprocess_sales_stock_rrp_rmb_task = PythonOperator(
     on_failure_callback = dag_failure_handler,
     dag = dag,
 )
-
-
 
 preprocess_sales_stock_rrp_rmb_task >> sales_stock_rrp_rmb_src2stg_task >> sales_stock_rrp_rmb_stg2ods_task >> postprocess_sales_stock_rrp_rmb_task
