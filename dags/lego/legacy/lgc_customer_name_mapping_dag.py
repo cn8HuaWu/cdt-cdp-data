f9b1072656d229f0c2852fc6fdae1b07dd43d2e8
cn8HelBa <80935853+cn8HelBa@users.noreply.github.com>
2021-03-23 14:38:54 +0800

    Add files via upload
    add edw's code

--- dags/lego/legacy/lgc_customer_name_mapping_dag.py
+++ dags/lego/legacy/lgc_customer_name_mapping_dag.py
@@ -82,6 +82,13 @@ def load_stg2ods(**kwargs):
     stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db, has_head = False )
     stg2ods.start()
 
+def load_ods2edw(**kwargs):
+    batch_date = kwargs.get('dag_run').conf.get('batch_date')
+    pkey = entity_conf[src_entity]["key"]
+    table_prefix = entity_conf[src_entity]["edw_prefix"]
+    update_type = entity_conf[src_entity]["update_type"]
+    ods2edw = Ods2edwHandler(batch_date, SRC_NAME, entity, pkey, table_prefix, myutil, db)
+    ods2edw.start()
 
 args = {
     'owner': 'cdp_admin',
@@ -117,6 +124,13 @@ customer_name_mapping_src2stg_task = PythonOperator(
     dag=dag,
 )
 
+customer_name_mapping_ods2edw_task = PythonOperator(
+    task_id='customer_name_mapping_ods2edw_task',
+    provide_context=True,
+    python_callable=load_ods2edw,
+    on_failure_callback=dag_failure_handler,
+    dag=dag,
+)
 
 customer_name_mapping_stg2ods_task = PythonOperator(
     task_id='customer_name_mapping_stg2ods_task',
@@ -136,4 +150,4 @@ postprocess_customer_name_mapping_task = PythonOperator(
     dag = dag,
 )
 
-preprocess_customer_name_mapping_task >> customer_name_mapping_src2stg_task >> customer_name_mapping_stg2ods_task >> postprocess_customer_name_mapping_task
+preprocess_customer_name_mapping_task >> customer_name_mapping_src2stg_task >> customer_name_mapping_stg2ods_task >> customer_name_mapping_ods2edw_task >> postprocess_customer_name_mapping_task
