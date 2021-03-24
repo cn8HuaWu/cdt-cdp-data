c02bd7d52b3dcbf7e1824607752e2ab3b6a7834e
cn8HelBa <80935853+cn8HelBa@users.noreply.github.com>
2021-03-24 17:05:27 +0800

    Add files via upload
    fix the bug

--- dags/lego/legacy/lgc_cs_weekly_phasing_dag.py
+++ dags/lego/legacy/lgc_cs_weekly_phasing_dag.py
@@ -28,7 +28,7 @@ TEMP_FOLDER = 'Temp'
 SRC_NAME = "lgc"
 STAGING = 'Staging'
 ODS = 'ODS'
-TEMP_FOLDER='Temp'
+TEMP_FOLDER = 'Temp'
 
 entity = 'cs_weekly_phasing'
 src_entity = 'lgc_cs_weekly_phasing'
@@ -37,56 +37,62 @@ DAG_NAME = 'lgc_cs_weekly_phasing_dag'
 myutil = Myutil(dag_home=DAG_HOME, entity_name=src_entity)
 db = myutil.get_db()
 entity_conf = myutil.get_entity_config()
-email_to_list =  Variable.get('email_to_list').split(',')
+email_to_list = Variable.get('email_to_list').split(',')
 
-def process_fileload(is_encrypted = False, is_compressed = False, **kwargs):
+
+def process_fileload(is_encrypted=False, is_compressed=False, **kwargs):
     logging.info("current path: " + os.getcwd())
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
+
     # remove the ok file and get the source file
-    if( OK_FILE_PATH is None or not os.path.exists(OK_FILE_PATH) ):
+    if (OK_FILE_PATH is None or not os.path.exists(OK_FILE_PATH)):
         logging.error("OK_FILE_PATH: %s, ok file does not exist. ", OK_FILE_PATH)
-        raise IOError("OK_FILE_PATH not found") 
-    # else:
+        raise IOError("OK_FILE_PATH not found")
+        # else:
     #     os.remove(OK_FILE_PATH)
 
-    if( not os.path.isfile(OK_FILE_PATH[:-3]) ):
+    if (not os.path.isfile(OK_FILE_PATH[:-3])):
         logging.error("Source file does not exist. File path: %s", OK_FILE_PATH[:-3])
         ## source file does not exist, set the Job failed
-        raise IOError("Source file not found") 
+        raise IOError("Source file not found")
 
-    myutil.modify_ok_file_prefix( old_prefix=None, prefix="running", ok_file_path=OK_FILE_PATH)
-    
-def post_process_fileload( **kwargs):
-    #rename: change prefix to "done-"
-    if ("skip_load"  in kwargs.get('dag_run').conf 
-        and kwargs.get('dag_run').conf.get("skip_load").upper() == 'Y' ):
-        return 
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
+    myutil.modify_ok_file_prefix(old_prefix=None, prefix="running", ok_file_path=OK_FILE_PATH)
+
+
+def post_process_fileload(**kwargs):
+    # rename: change prefix to "done-"
+    if ("skip_load" in kwargs.get('dag_run').conf
+            and kwargs.get('dag_run').conf.get("skip_load").upper() == 'Y'):
+        return
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
     myutil.modify_ok_file_prefix("running", "done", OK_FILE_PATH)
 
+
 def dag_failure_handler(context):
-    #rename: change prefix to "failed-"
-    OK_FILE_PATH  = context.get('dag_run').conf.get('ok_file_path')
-    myutil.modify_ok_file_prefix("running", "failed",OK_FILE_PATH)
-    
+    # rename: change prefix to "failed-"
+    OK_FILE_PATH = context.get('dag_run').conf.get('ok_file_path')
+    myutil.modify_ok_file_prefix("running", "failed", OK_FILE_PATH)
+
+
 def load_src2stg(**kwargs):
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
     src_filename = kwargs.get('dag_run').conf.get('src_filename')
     #
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, has_head=False, sheetname='Sheet1', merge =False)
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
+    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH,
+                             has_head=False, sheetname='Sheet1', merge=False)
     src2stg.start(version='v2')
 
-def load_stg2ods(**kwargs):
 
+def load_stg2ods(**kwargs):
     pkey = entity_conf[src_entity]["key"]
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
-    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db, has_head = False )
+    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db,
+                             has_head=False)
     stg2ods.start()
 
 
@@ -101,35 +107,34 @@ args = {
     "src_name": SRC_NAME
 }
 
-dag = DAG(dag_id = DAG_NAME,
-            default_args = args,
-            concurrency = 5, 
-            max_active_runs = 1, 
-            schedule_interval = None)
+dag = DAG(dag_id=DAG_NAME,
+          default_args=args,
+          concurrency=5,
+          max_active_runs=1,
+          schedule_interval=None)
 
 preprocess_cs_weekly_phasing_task = PythonOperator(
-    task_id = 'preprocess_cs_weekly_phasing_task',
-    provide_context = True,
-    python_callable = process_fileload,
-    op_kwargs = {'is_encrypted': False},
-    on_failure_callback = dag_failure_handler,
-    dag = dag,
+    task_id='preprocess_cs_weekly_phasing_task',
+    provide_context=True,
+    python_callable=process_fileload,
+    op_kwargs={'is_encrypted': False},
+    on_failure_callback=dag_failure_handler,
+    dag=dag,
 )
 
 cs_weekly_phasing_src2stg_task = PythonOperator(
     task_id='cs_weekly_phasing_src2stg_task',
-    provide_context = True,
-    python_callable = load_src2stg,
-    on_failure_callback = dag_failure_handler,
+    provide_context=True,
+    python_callable=load_src2stg,
+    on_failure_callback=dag_failure_handler,
     dag=dag,
 )
 
-
 cs_weekly_phasing_stg2ods_task = PythonOperator(
     task_id='cs_weekly_phasing_stg2ods_task',
-    provide_context = True,
-    python_callable = load_stg2ods,
-    on_failure_callback = dag_failure_handler,
+    provide_context=True,
+    python_callable=load_stg2ods,
+    on_failure_callback=dag_failure_handler,
     dag=dag,
 )
 
@@ -167,13 +172,13 @@ edw_lgc_cs_week_phase_insert = PythonOperator(
 )
 
 postprocess_cs_weekly_phasing_task = PythonOperator(
-    task_id = 'postprocess_cs_weekly_phasing_task',
-    provide_context = True,
-    python_callable = post_process_fileload,
-    op_kwargs = {'is_encrypted': False},
-    on_failure_callback = dag_failure_handler,
-    dag = dag,
+    task_id='postprocess_cs_weekly_phasing_task',
+    provide_context=True,
+    python_callable=post_process_fileload,
+    op_kwargs={'is_encrypted': False},
+    on_failure_callback=dag_failure_handler,
+    dag=dag,
 )
 
-preprocess_cs_weekly_phasing_task >> cs_weekly_phasing_src2stg_task >> cs_weekly_phasing_stg2ods_task >> lgc_cs_week_phase_create
-lgc_cs_week_phase_create >> edw_lgc_cs_week_phase_delete >> edw_lgc_cs_week_phase_insert >> postprocess_cs_weekly_phasing_task
+preprocess_cs_weekly_phasing_task >> cs_weekly_phasing_src2stg_task >> cs_weekly_phasing_stg2ods_task >> edw_lgc_cs_week_phase_create
+edw_lgc_cs_week_phase_create >> edw_lgc_cs_week_phase_delete >> edw_lgc_cs_week_phase_insert >> postprocess_cs_weekly_phasing_task
