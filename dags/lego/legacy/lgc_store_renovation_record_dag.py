36d0a4035fb82a6c5a954ab282441e4bdc1c446b
cn8HelBa <helen.bao@consultant.lego.com>
2021-04-08 14:08:21 +0800

    add code
    add "excel_fun_list"

--- dags/lego/legacy/lgc_store_renovation_record_dag.py
+++ dags/lego/legacy/lgc_store_renovation_record_dag.py
@@ -70,7 +70,8 @@ def load_src2stg(**kwargs):
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH,  has_head=False, sheetname='Sheet1', merge =False)
+    excel_fun_list = [myutil.rearrange_columns]
+    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, has_head=False, sheetname='Sheet1', merge =False)
     src2stg.start(version='v2')
 
 def load_stg2ods(**kwargs):
