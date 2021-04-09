c59ecc43090d586ec9e77667b0b846cdf0cd8664
cn8HelBa <80935853+cn8HelBa@users.noreply.github.com>
2021-04-09 10:36:53 +0800

    add has_head =1  when 2ods

--- dags/lego/legacy/lgc_sales_stock_nip_dkk_dag.py
+++ dags/lego/legacy/lgc_sales_stock_nip_dkk_dag.py
@@ -91,7 +91,7 @@ def load_stg2ods(**kwargs):
     #
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
     stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db,
-                             has_head=False)
+                             has_head=1)
     stg2ods.start()
 
 args = {
