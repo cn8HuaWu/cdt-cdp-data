cdb09647b8fa1291d4b67cc07dfa0e257db0f4f3
cn8HelBa <helen.bao@consultant.lego.com>
2021-04-12 19:14:12 +0800

    fix the bug
    fix the locationof "** sheet"

--- dags/lego/legacy/lgc_store_closure_list_dag.py
+++ dags/lego/legacy/lgc_store_closure_list_dag.py
@@ -82,7 +82,7 @@ def load_src2stg(**kwargs):
     OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
     excel_fun_list = [myutil.rearrange_columns]
     src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, 
-                             has_head=False, sheetname='Sheet1', merge=False)
+                             has_head=False, sheetname='Sheet1', merge=False, **sheet)
     src2stg.start(version='v2')
 
 
@@ -92,7 +92,7 @@ def load_stg2ods(**kwargs):
     #
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
     stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db,
-                             has_head=False, **sheet)
+                             has_head=False)
     stg2ods.start()
 
 
