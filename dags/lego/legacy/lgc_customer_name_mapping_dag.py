a20db57e66cd3bcee86063be881bc3f313941e62
cn8HelBa <helen.bao@consultant.lego.com>
2021-04-26 08:45:03 +0800

    update "customer_name_mapping"
    1. source sheet = 'Mapping_CustomerName"
    2. rename edw table name
     to "f_lgc_customer_name_mapping"

--- dags/lego/legacy/lgc_customer_name_mapping_dag.py
+++ dags/lego/legacy/lgc_customer_name_mapping_dag.py
@@ -6,7 +6,7 @@ from airflow import DAG
 import logging
 import os, sys
 
-DAG_HOME =  Variable.get('dag_home').strip().rstrip('/')
+DAG_HOME = Variable.get('dag_home').strip().rstrip('/')
 sys.path.append("../tasks/")
 sys.path.append(DAG_HOME + "/tasks/")
 
@@ -17,79 +17,86 @@ from utils.myutil import Myutil
 from utils.db import Mydb
 from update_downstream_table import update_downstream
 
-
 # variable to run the shell scripts
 SRC_NAME = "lgc"
 STAGING = 'Staging'
 ODS = 'ODS'
-TEMP_FOLDER='Temp'
+TEMP_FOLDER = 'Temp'
 
 entity = 'customer_name_mapping'
 src_entity = 'lgc_customer_name_mapping'
 DAG_NAME = 'lgc_customer_name_mapping_dag'
 
-sheet ={
-"Sheet1":{
-    'start_column':0,
-    'column_width':2
-}
+sheet = {
+    "Mapping_CustomerName": {
+        'start_column': 0,
+        'column_width': 2
+    }
 }
 
 myutil = Myutil(dag_home=DAG_HOME, entity_name=src_entity)
 db = myutil.get_db()
 entity_conf = myutil.get_entity_config()
-email_to_list =  Variable.get('email_to_list').split(',')
+email_to_list = Variable.get('email_to_list').split(',')
 
-def process_fileload(is_encrypted = False, is_compressed = False, **kwargs):
+
+def process_fileload(is_encrypted=False, is_compressed=False, **kwargs):
     logging.info("current path: " + os.getcwd())
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
-    
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
+
     # remove the ok file and get the source file
-    if( OK_FILE_PATH is None or not os.path.exists(OK_FILE_PATH) ):
+    if (OK_FILE_PATH is None or not os.path.exists(OK_FILE_PATH)):
         logging.error("OK_FILE_PATH: %s, ok file does not exist. ", OK_FILE_PATH)
-        raise IOError("OK_FILE_PATH not found") 
-    # else:
+        raise IOError("OK_FILE_PATH not found")
+        # else:
     #     os.remove(OK_FILE_PATH)
 
-    if( not os.path.isfile(OK_FILE_PATH[:-3]) ):
+    if (not os.path.isfile(OK_FILE_PATH[:-3])):
         logging.error("Source file does not exist. File path: %s", OK_FILE_PATH[:-3])
         ## source file does not exist, set the Job failed
-        raise IOError("Source file not found") 
+        raise IOError("Source file not found")
 
-    myutil.modify_ok_file_prefix( old_prefix=None, prefix="running", ok_file_path=OK_FILE_PATH)
-    
-def post_process_fileload( **kwargs):
-    #rename: change prefix to "done-"
-    if ("skip_load"  in kwargs.get('dag_run').conf 
-        and kwargs.get('dag_run').conf.get("skip_load").upper() == 'Y' ):
-        return 
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
+    myutil.modify_ok_file_prefix(old_prefix=None, prefix="running", ok_file_path=OK_FILE_PATH)
+
+
+def post_process_fileload(**kwargs):
+    # rename: change prefix to "done-"
+    if ("skip_load" in kwargs.get('dag_run').conf
+            and kwargs.get('dag_run').conf.get("skip_load").upper() == 'Y'):
+        return
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
     myutil.modify_ok_file_prefix("running", "done", OK_FILE_PATH)
 
+
 def dag_failure_handler(context):
-    #rename: change prefix to "failed-"
-    OK_FILE_PATH  = context.get('dag_run').conf.get('ok_file_path')
-    myutil.modify_ok_file_prefix("running", "failed",OK_FILE_PATH)
-    
+    # rename: change prefix to "failed-"
+    OK_FILE_PATH = context.get('dag_run').conf.get('ok_file_path')
+    myutil.modify_ok_file_prefix("running", "failed", OK_FILE_PATH)
+
+
 def load_src2stg(**kwargs):
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
     src_filename = kwargs.get('dag_run').conf.get('src_filename')
     #
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
-    OK_FILE_PATH  = kwargs.get('dag_run').conf.get('ok_file_path')
+    OK_FILE_PATH = kwargs.get('dag_run').conf.get('ok_file_path')
     excel_fun_list = [myutil.rearrange_columns]
-    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH, excel_fun_list=excel_fun_list, has_head=False, sheetname='Sheet1', merge =False,**sheet)
+    src2stg = Src2stgHandler(STAGING, batch_date, SRC_NAME, entity, stg_suffix, src_filename, myutil, OK_FILE_PATH,
+                             excel_fun_list=excel_fun_list, has_head=False, sheetname='Mapping_CustomerName',
+                             merge=False, **sheet)
     src2stg.start(version='v2')
 
+
 def load_stg2ods(**kwargs):
-    
     pkey = entity_conf[src_entity]["key"]
     stg_suffix = entity_conf[src_entity]["stg_suffix"]
     #
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
-    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db, has_head = False )
+    stg2ods = Stg2odsHandler(TEMP_FOLDER, STAGING, ODS, batch_date, SRC_NAME, entity, stg_suffix, pkey, myutil, db,
+                             has_head=False)
     stg2ods.start()
+
 
 def load_ods2edw(**kwargs):
     batch_date = kwargs.get('dag_run').conf.get('batch_date')
@@ -98,6 +105,7 @@ def load_ods2edw(**kwargs):
     update_type = entity_conf[src_entity]["update_type"]
     ods2edw = Ods2edwHandler(batch_date, SRC_NAME, entity, pkey, table_prefix, myutil, db)
     ods2edw.start()
+
 
 args = {
     'owner': 'cdp_admin',
@@ -110,37 +118,36 @@ args = {
     "src_name": SRC_NAME
 }
 
-dag = DAG(dag_id = DAG_NAME,
-            default_args = args,
-            concurrency = 5, 
-            max_active_runs = 1, 
-            schedule_interval = None)
+dag = DAG(dag_id=DAG_NAME,
+          default_args=args,
+          concurrency=5,
+          max_active_runs=1,
+          schedule_interval=None)
 
 preprocess_customer_name_mapping_task = PythonOperator(
-    task_id = 'preprocess_customer_name_mapping_task',
-    provide_context = True,
-    python_callable = process_fileload,
-    op_kwargs = {'is_encrypted': False},
-    on_failure_callback = dag_failure_handler,
-    dag = dag,
+    task_id='preprocess_customer_name_mapping_task',
+    provide_context=True,
+    python_callable=process_fileload,
+    op_kwargs={'is_encrypted': False},
+    on_failure_callback=dag_failure_handler,
+    dag=dag,
 )
 
 lgc_customer_name_mapping_src2stg_task = PythonOperator(
     task_id='lgc_customer_name_mapping_src2stg_task',
-    provide_context = True,
-    python_callable = load_src2stg,
-    on_failure_callback = dag_failure_handler,
+    provide_context=True,
+    python_callable=load_src2stg,
+    on_failure_callback=dag_failure_handler,
     dag=dag,
 )
 
 lgc_customer_name_mapping_stg2ods_task = PythonOperator(
     task_id='lgc_customer_name_mapping_stg2ods_task',
-    provide_context = True,
-    python_callable = load_stg2ods,
-    on_failure_callback = dag_failure_handler,
+    provide_context=True,
+    python_callable=load_stg2ods,
+    on_failure_callback=dag_failure_handler,
     dag=dag,
 )
-
 
 # create edw data task:
 edw_lgc_customer_name_map_create = PythonOperator(
@@ -175,15 +182,13 @@ edw_lgc_customer_name_map_insert = PythonOperator(
     dag=dag,
 )
 
-
-
 postprocess_customer_name_mapping_task = PythonOperator(
-    task_id = 'postprocess_customer_name_mapping_task',
-    provide_context = True,
-    python_callable = post_process_fileload,
-    op_kwargs = {'is_encrypted': False},
-    on_failure_callback = dag_failure_handler,
-    dag = dag,
+    task_id='postprocess_customer_name_mapping_task',
+    provide_context=True,
+    python_callable=post_process_fileload,
+    op_kwargs={'is_encrypted': False},
+    on_failure_callback=dag_failure_handler,
+    dag=dag,
 )
 
 preprocess_customer_name_mapping_task >> lgc_customer_name_mapping_src2stg_task >> lgc_customer_name_mapping_stg2ods_task >> edw_lgc_customer_name_map_create
